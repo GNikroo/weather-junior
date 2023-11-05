8fc67a0a12e973a75325b0b067ebcfa2b9873a6a
Gina Nikroo <gina.nikroo@gmail.com>
2023-11-05 21:17:20 +0100

    Provide data for image display

---
+++ kids-weather/src/data.js
@@ -0,0 +1,47 @@
+import img1 from "./assets/clothing/Child.png";
+import img2 from "./assets/clothing/Summer.png";
+import img3 from "./assets/clothing/Chilly.png";
+import img4 from "./assets/clothing/Raingear.png";
+import img5 from "./assets/clothing/Wintergear.png";
+import img6 from "./assets/clothing/Windy.png";
+
+export const outfits = [
+  {
+    description: "Ready for the day!",
+    items: "",
+    image: img1,
+    alt: "child dressed in pajamas",
+  },
+  {
+    description: "Today is warm! You might need sunscreen and a cap.",
+    items: "sunscreen, cap",
+    image: img2,
+    alt: "child dressed for summer",
+  },
+  {
+    description: "Today is chilly! You'll need a sweater and maybe a jacket.",
+    items: "sweater, jacket, shoes, hat",
+    image: img3,
+    alt: "child dressed for chill",
+  },
+  {
+    description: "Today is windy! You'll need a jacket, gloves and a hat.",
+    items: "sweater, jacket, shoes, hat",
+    image: img6,
+    alt: "child dressed for chilly wind",
+  },
+  {
+    description:
+      "Today is chilly and wet! You'll need rain gear and boots. Don't forget gloves and a hat!",
+    items: "rain pants, rain jacket, hat, rain boots, gloves, hat",
+    image: img4,
+    alt: "child dressed for rain",
+  },
+  {
+    description:
+      "Today is cold! You'll need a snowsuit or thick jacket and pants. Don't forget gloves and a hat!",
+    items: "jacket, pants, snowsuit, boots, gloves, hat",
+    image: img5,
+    alt: "child dressed for cold",
+  },
+];
