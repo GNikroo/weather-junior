88347921b583cfabfb62968433f4365f97796aa4
Gina Nikroo <gina.nikroo@gmail.com>
2023-11-07 13:55:48 +0100

    Update CurrentWeather to fetch user's location input data and display to user

--- kids-weather/src/components/pages/CurrentWeather.js
+++ kids-weather/src/components/pages/CurrentWeather.js
@@ -1,27 +1,69 @@
 import React, { useState, useEffect } from "react";
-import Outfits from "../Outfits";
+import axios from "axios";
+import { weatherConditions } from "../../data";
 
 const CurrentWeather = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
+  const [weatherCode, setWeatherCode] = useState(null);
+  const [location, setLocation] = useState(
+    localStorage.getItem("recentLocation") || "Stockholm"
+  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLocationChange = (e) => {
+    const newLocation = e.target.value;
+    setLocation(newLocation);
+    localStorage.setItem("recentLocation", newLocation);
+  };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 60000); // Update every minute (60000 milliseconds)
+    const fetchWeatherData = async () => {
+      try {
+        const accessKey = "2280b70e2aed37d964bcbced0c1a79e3";
+        const weatherResponse = await axios.get(
+          `http://api.weatherstack.com/current?access_key=${accessKey}&query=${location}`
+        );
 
-    return () => {
-      clearInterval(interval);
+        if (weatherResponse.data && weatherResponse.data.current) {
+          console.log("Weather Data:", weatherResponse.data);
+
+          const currentWeatherCode = weatherResponse.data.current.weather_code;
+          setWeatherCode(currentWeatherCode);
+        } else {
+          console.error("Weather data not found");
+        }
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      } finally {
+        setIsLoading(false);
+      }
     };
-  }, []);
+    fetchWeatherData();
+  }, [location]);
 
-  const getFormattedTime = (date) => {
-    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  const getWeatherIcon = (code) => {
+    for (const condition of Object.values(weatherConditions)) {
+      if (condition[code] && condition[code].hasOwnProperty("icon")) {
+        return condition[code].icon;
+      }
+    }
+
+    return "default-icon";
   };
 
   return (
     <div>
-      <p>Current Time: {getFormattedTime(currentTime)}</p>
-      <Outfits />
+      <input type="text" value={location} onChange={handleLocationChange} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        weatherCode && (
+          <img
+            src={getWeatherIcon(weatherCode)}
+            height="100"
+            alt="Weather Icon"
+          />
+        )
+      )}
     </div>
   );
 };
