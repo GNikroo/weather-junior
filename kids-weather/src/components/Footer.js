42a22bf7213cc412c03b8d3c0f05ceab5c1943d6
Gina Nikroo <gina.nikroo@gmail.com>
2023-11-08 13:22:40 +0100

    Update fonts

--- kids-weather/src/components/Footer.js
+++ kids-weather/src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Card, Container } from "react-bootstrap";
 import appStyles from "../App.module.css";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <Container className={`${appStyles.Section} text-center`}>
-      <Card.Footer className="fixed-bottom">
+      <Card.Footer className={`${styles.Footer} fixed-bottom`}>
         <p>Gina Nikroo &copy; {currentYear} All rights reserved.</p>
       </Card.Footer>
     </Container>
