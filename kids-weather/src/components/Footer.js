6dc576a72e78ee2f5bd2e6e1fd318d10a5d7d4f9
Gina Nikroo <gina.nikroo@gmail.com>
2023-11-08 10:06:28 +0100

    Update footer with copyright

--- kids-weather/src/components/Footer.js
+++ kids-weather/src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Card, Container } from "react-bootstrap";
+import appStyles from "../App.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <Container className={`${appStyles.Section} text-center`}>
+      <Card.Footer className="fixed-bottom">
+        <p>Gina Nikroo &copy; {currentYear} All rights reserved.</p>
+      </Card.Footer>
+    </Container>
+  );
 };
 
 export default Footer;
