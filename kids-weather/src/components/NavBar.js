9dbd8bdf0fbd73a2b302f02a588b7897390f39db
Gina Nikroo <gina.nikroo@gmail.com>
2023-11-08 13:26:05 +0100

    Style NavBar and temporarily hide dropdown menu.

--- kids-weather/src/components/NavBar.js
+++ kids-weather/src/components/NavBar.js
@@ -1,28 +1,34 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styles from "../styles/NavBar.module.css";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBar = () => {
   return (
-    <Navbar>
-      <Link to="/today" className="text-decoration-none">
-        Kid's Daily Forecast
-      </Link>
-      <Nav>
-        <Link to="/about" className={styles.Links}>
-          About
+    <Container>
+      <Navbar className={styles.Section}>
+        <Link to="/today" className={`${styles.Brand} text-decoration-none`}>
+          Weather Jr.
         </Link>
-        <br></br>
-        <Link to="/today" className={styles.Links}>
-          Current Weather
-        </Link>{" "}
-        <br></br>
-        <Link to="/past" className={styles.Links}>
-          Historical Weather
-        </Link>
-      </Nav>
-    </Navbar>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className={styles.Dropdown} id="basic-navbar-nav">
+          <NavDropdown title="" id="basic-nav-dropdown">
+            <Link to="/about" className={styles.Links}>
+              About
+            </Link>
+            <br></br>
+            <Link to="/today" className={styles.Links}>
+              Current Weather
+            </Link>
+            <br></br>
+            <Link to="/past" className={styles.Links}>
+              Historical Weather
+            </Link>
+          </NavDropdown>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
   );
 };
 
