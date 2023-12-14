836c483b3d7a971e7f1ca31690a1d776a2ed1887
Gina Nikroo <gina.nikroo@gmail.com>
2023-12-14 14:54:14 +0100

    Create ScreenSizeChecker

---
+++ src/components/ScreenSizeChecker.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const ScreenSizeChecker = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return { isSmallScreen };
+};
+
+export default ScreenSizeChecker;
