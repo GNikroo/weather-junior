61b469bdaf746281130824d311f796f0be950121
Gina Nikroo <gina.nikroo@gmail.com>
2023-12-15 09:22:58 +0100

    Move search bar to NavBar with useWeatherStore for state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,55 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Navbar, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styles from "../styles/NavBar.module.css";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import useWeatherStore from "./hooks/useWeatherStore";
 
 const NavBar = () => {
+  const { handleLocationChange, inputLocation } = useWeatherStore();
   return (
     <Container>
       <Navbar className={styles.Section}>
-        <Link to="/" className={`${styles.Brand} text-decoration-none`}>
-          Weather Jr.
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className={styles.Dropdown} id="basic-navbar-nav">
-          <NavDropdown title="" id="basic-nav-dropdown">
-            <Link to="/about" className={styles.Links}>
-              About
-            </Link>
-            <br></br>
-            <Link to="/today" className={styles.Links}>
-              Current Weather
-            </Link>
-            <br></br>
-            <Link to="/past" className={styles.Links}>
-              Historical Weather
-            </Link>
-          </NavDropdown>
-        </Navbar.Collapse>
+        <Col xs={4} sm={5} md={6}>
+          <Link to="/" className={`${styles.Brand} text-decoration-none`}>
+            Weather Jr.
+          </Link>
+        </Col>
+        <Col
+          className={`${styles.InputContainer} d-flex input-group text-center`}
+        >
+          <span
+            className={`${styles.InputSearch} input-group-text`}
+            id="basic-addon1"
+          >
+            🔎
+          </span>
+          <input
+            type="text"
+            value={inputLocation}
+            onChange={handleLocationChange}
+            className={`${styles.Input}`}
+            aria-describedby="basic-addon1"
+          />
+        </Col>
+        <Col xs={1}>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className={styles.Dropdown} id="basic-navbar-nav">
+            <NavDropdown title="" id="basic-nav-dropdown">
+              <Link to="/about" className={styles.Links}>
+                About
+              </Link>
+              <br></br>
+              <Link to="/today" className={styles.Links}>
+                Current Weather
+              </Link>
+              <br></br>
+              <Link to="/past" className={styles.Links}>
+                Historical Weather
+              </Link>
+            </NavDropdown>
+          </Navbar.Collapse>
+        </Col>
       </Navbar>
     </Container>
   );
