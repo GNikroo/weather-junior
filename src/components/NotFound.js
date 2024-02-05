12cd59df94adccf4b0f4eae2fdd79ea4a3361d0e
Gina Nikroo <gina.nikroo@gmail.com>
2024-02-05 15:54:44 +0100

    Update not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "../styles/NotFound.module.css";
+import { Container, Row } from "react-bootstrap";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Container className="d-flex">
+      <Row className={`${styles.NotFound}`}>
+        <div className="align-self-center">
+          <span className="d-flex justify-content-center">
+            Looks like you're lost!
+          </span>
+          <span className="d-flex justify-content-center">
+            Try going back to the&nbsp;<a href="/">beginning</a>.
+          </span>
+        </div>
+      </Row>
+    </Container>
+  );
 };
 
 export default NotFound;
