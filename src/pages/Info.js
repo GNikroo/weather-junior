54d3589ff06cf28c5a56aafdfd8d90d366a35158
Gina Nikroo <gina.nikroo@gmail.com>
2024-02-09 15:22:55 +0100

    Remove link from Info to prevent redirecting child users. Remove dropdown to be more clear

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -4,44 +4,25 @@ import styles from "../styles/Info.module.css";
 
 const Info = () => {
   return (
-    <div className={`${styles.Info} p-2`}>
+    <div className={`${styles.Info} p-3`}>
       <div className="d-flex flex-column">
-        <Dropdown className="p-0 m-0">
-          <Dropdown.Toggle className="ABOUT" id="dropdown-basic">
-            About Weather Jr.
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.ItemText>
-              <p>
-                Weather Jr. is designed to provide weather information for
-                children. It aims to present weather data in an engaging and
-                pedagogical way, making it easier for children to understand and
-                interact with.
-              </p>
-              <p>
-                The app includes colorful visuals and simplified weather
-                descriptions while providing educational content related to
-                weather phenomena.
-              </p>
-              <p>Explore and enjoy!</p>
-            </Dropdown.ItemText>
-          </Dropdown.Menu>
-        </Dropdown>
-        <Dropdown className="mt-4">
-          <Dropdown.Toggle id="dropdown-basic">Contact</Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.ItemText>
-              <p>
-                If you have suggestions, comments, or questions, feel free to
-                contact me. I will get back to you as soon as possible{" "}
-                <a href="mailto:info.weatherjr@gmail.com">
-                  info.weatherjr@gmail.com
-                </a>
-                .
-              </p>
-            </Dropdown.ItemText>
-          </Dropdown.Menu>
-        </Dropdown>
+        <h2>About Weather Jr.</h2>
+        <p>
+          Weather Jr. is designed to provide weather information for children.
+          It aims to present weather data in an engaging and pedagogical way,
+          making it easier for children to understand and interact with.
+        </p>
+        <p>
+          The app includes colorful visuals and simplified weather descriptions
+          while providing educational content related to weather phenomena.
+        </p>
+        <p>Explore and enjoy!</p>
+        <h2>Contact</h2>
+        <p>
+          If you have suggestions, comments, or questions, feel free to contact
+          me. I will get back to you as soon as possible
+          info.weatherjr@gmail.com
+        </p>
       </div>
     </div>
   );
